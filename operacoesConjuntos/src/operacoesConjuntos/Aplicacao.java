0b8b0170faafc2169ddaea9c859f2a01be9ab6e0
anasantos11 <anasantos1103@gmail.com>
2017-11-22 16:55:09 -0200

    Alterado tipo dos atributos e adicionado operação do conjunto das partes

--- operacoesConjuntos/src/operacoesConjuntos/Aplicacao.java
+++ operacoesConjuntos/src/operacoesConjuntos/Aplicacao.java
@@ -16,13 +16,14 @@ public class Aplicacao {
 		conjunto = JOptionPane.showInputDialog(null,
 				"Insira os elementos do Conjunto B. \n \n ATENÇÃO - Os elementos devem ser separados por espaço! \n \n");
 		conjuntos.criarConjunto(conjunto, 2);
-				
+
 		conjuntos.calcularUniao();
 		conjuntos.calcularInterseccao();
 		conjuntos.calcularDiferencaSimetrica();
 		conjuntos.calcularDiferenca();
 		conjuntos.calcularProdutoCartesiano();
 		conjuntos.calcularComplementar();
+		conjuntos.calcularPartes();
 			
 		System.out.println(
 				"Conjunto A = " + conjuntos.getConjuntoA() + "\n" + 
@@ -33,7 +34,8 @@ public class Aplicacao {
 				"Produto Cartesiano = " + conjuntos.getProdutoCartesiano() + "  Quant. elementos: " + conjuntos.getProdutoCartesiano().size()+ "\n" + 
 				"Diferença Simétrica = " + conjuntos.getDiferencaSimetrica() + "  Quant. elementos: " + conjuntos.getDiferencaSimetrica().size() + "\n" + 
 				"Diferença A - B = " + conjuntos.getDiferenca() + "  Quant. elementos: " + conjuntos.getDiferenca().size() + "\n" + 
-				"Complementar = " + conjuntos.getComplementar() + "  Quant. elementos: " + conjuntos.getComplementar().size()
+				"Complementar = " + conjuntos.getComplementar() + "  Quant. elementos: " + conjuntos.getComplementar().size() + "\n" + 
+				"Partes = " + conjuntos.getPartes() + "  Quant. elementos: " + conjuntos.getPartes().size()
 		);
 				
 	}
--- operacoesConjuntos/src/operacoesConjuntos/Conjuntos.java
+++ operacoesConjuntos/src/operacoesConjuntos/Conjuntos.java
@@ -1,35 +1,36 @@
 package operacoesConjuntos;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Conjuntos {
 	private List<Character> alfabeto;
 	private String par;
-	private LinkedHashSet<String> conjuntoUniverso;
-	private LinkedHashSet<String> conjuntoA;
-	private LinkedHashSet<String> conjuntoB;
-	private LinkedHashSet<String> uniao;
-	private LinkedHashSet<String> interseccao;
-	private LinkedHashSet<String> diferencaSimetrica;
-	private LinkedHashSet<String> diferenca;
-	private LinkedHashSet<String> produtoCartesiano;
-	private LinkedHashSet<String> complementar;
-	private LinkedHashSet<String> partes;
-	
+	private HashSet<String> conjuntoUniverso;
+	private HashSet<String> conjuntoA;
+	private HashSet<String> conjuntoB;
+	private HashSet<String> uniao;
+	private HashSet<String> interseccao;
+	private HashSet<String> diferencaSimetrica;
+	private HashSet<String> diferenca;
+	private HashSet<String> produtoCartesiano;
+	private HashSet<String> complementar;
+	private Set<Set<String>> partes;
+
 	public Conjuntos() {
 		this.alfabeto = new ArrayList<Character>();
-		this.conjuntoUniverso = new LinkedHashSet<String>();
-		this.conjuntoA = new LinkedHashSet<String>();
-		this.conjuntoB = new LinkedHashSet<String>();
-		this.uniao = new LinkedHashSet<String>();
-		this.interseccao = new LinkedHashSet<String>();
-		this.diferencaSimetrica = new LinkedHashSet<String>();
-		this.diferenca = new LinkedHashSet<String>();
-		this.produtoCartesiano = new LinkedHashSet<String>();
-		this.complementar = new LinkedHashSet<String>();
-		this.partes = new LinkedHashSet<String>();
+		this.conjuntoUniverso = new HashSet<String>();
+		this.conjuntoA = new HashSet<String>();
+		this.conjuntoB = new HashSet<String>();
+		this.uniao = new HashSet<String>();
+		this.interseccao = new HashSet<String>();
+		this.diferencaSimetrica = new HashSet<String>();
+		this.diferenca = new HashSet<String>();
+		this.produtoCartesiano = new HashSet<String>();
+		this.complementar = new HashSet<String>();
+		this.partes = new HashSet<Set<String>>();
 
 	}
 
@@ -88,7 +89,7 @@ public class Conjuntos {
 		for (String a : conjuntoA) {
 			for (String b : conjuntoB) {
 				par = "(" + a + "," + b + ")";
-				produtoCartesiano.add( par);
+				produtoCartesiano.add(par);
 			}
 		}
 	}
@@ -117,49 +118,85 @@ public class Conjuntos {
 
 	}
 
+
 	public void calcularPartes() {
-		LinkedHashSet<String> teste = new LinkedHashSet<String>();
-		LinkedHashSet<String> list = new LinkedHashSet<>();
-		list.addAll(teste);
-
-		partes.addAll(conjuntoA);
-		partes.addAll(produtoCartesiano);
-
-		for (String a : conjuntoA) {
-			list.addAll(teste);
-		}
+		partes = calcularPartes(conjuntoA);
 	}
 
-	public LinkedHashSet<String> getConjuntoA() {
+	public Set<Set<String>> calcularPartes(Set<String> originalSet) {
+		Set<Set<String>> sets = new HashSet<Set<String>>();
+		if (originalSet.isEmpty()) {
+			sets.add(new HashSet<String>());
+			return sets;
+		}
+
+		List<String> list = new ArrayList<String>(originalSet);
+
+		String head = list.get(0);
+		Set<String> rest = new HashSet<String>(list.subList(1, list.size()));
+		for (Set<String> set : calcularPartes(rest)) {
+			Set<String> newSet = new HashSet<String>();
+			newSet.add(head);
+			newSet.addAll(set);
+			sets.add(newSet);
+			sets.add(set);
+		}
+		return sets;
+	}
+
+	public HashSet<String> conjuntoDasPartes() {
+		HashSet<String> conjuntoPartes = new HashSet<String>();
+
+		final int tamanho = conjuntoA.size();
+
+		for (int i = 0; i < (1 << tamanho); i++) {
+			StringBuilder sb = new StringBuilder("{");
+
+			for (int j = 0; j < tamanho; j++)
+				if ((i & (1 << j)) > 0)
+					sb.append(conjuntoA.toArray()[j] + ",");
+
+			sb.append("}");
+			conjuntoPartes.add(sb.toString().replace(",}", "}"));
+		}
+
+		return conjuntoPartes;
+	}
+
+	public HashSet<String> getConjuntoA() {
 		return conjuntoA;
 	}
 
-	public LinkedHashSet<String> getConjuntoB() {
+	public HashSet<String> getConjuntoB() {
 		return conjuntoB;
 	}
 
-	public LinkedHashSet<String> getUniao() {
+	public HashSet<String> getUniao() {
 		return uniao;
 	}
 
-	public LinkedHashSet<String> getInterseccao() {
+	public HashSet<String> getInterseccao() {
 		return interseccao;
 	}
 
-	public LinkedHashSet<String> getDiferencaSimetrica() {
+	public HashSet<String> getDiferencaSimetrica() {
 		return diferencaSimetrica;
 	}
 
-	public LinkedHashSet<String> getDiferenca() {
+	public HashSet<String> getDiferenca() {
 		return diferenca;
 	}
 
-	public LinkedHashSet<String> getProdutoCartesiano() {
+	public HashSet<String> getProdutoCartesiano() {
 		return produtoCartesiano;
 	}
 
-	public LinkedHashSet<String> getComplementar() {
+	public HashSet<String> getComplementar() {
 		return complementar;
+	}
+
+	public Set<Set<String>> getPartes() {
+		return partes;
 	}
 
 }
